54266fe4e930292c79cda5ca56101a9736c6994a
msuski <01187288@pw.edu.pl>
2025-03-02 21:12:26 +0100

    Implemented function to check location permissions

--- app/src/main/java/com/gps_usage/showCoordinates/ShowCoordinatesComponent.kt
+++ app/src/main/java/com/gps_usage/showCoordinates/ShowCoordinatesComponent.kt
@@ -1,6 +1,8 @@
 package com.gps_usage.showCoordinates
 
 import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -8,4 +10,44 @@ class ShowCoordinatesComponent(context: Context) {
     var fusedLocationProviderClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
     var latitude: Float = 0f
     var longitude: Float = 0f
+    var permissionContext: Context = context
+
+    fun getCurrentLocation() {
+        if(checkPermissions()) {
+            if(isLocationEnabled()) {
+                // get location
+            } else {
+                // return result
+            }
+        } else {
+            // return result
+        }
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+
+    private fun checkPermissions(): Boolean {
+        return (
+            ActivityCompat.checkSelfPermission(
+                permissionContext,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+            &&
+            ActivityCompat.checkSelfPermission(
+                permissionContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+//            &&
+//            ActivityCompat.checkSelfPermission(
+//                permissionContext,
+//                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+
+    }
 }
