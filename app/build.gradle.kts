77719a687ac19e1199d2e1f911bb348073242b54
msuski <01187288@pw.edu.pl>
2025-03-02 20:35:10 +0100

    Added dependencies to google location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(libs.google.location)
+//    implementation("com.google.android.gms:play-services-location:21.3.0")
 }
