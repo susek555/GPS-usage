bc3bbb8547fa5685f17a3f0655eb169685347b60
msuski <01187288@pw.edu.pl>
2025-03-04 20:03:38 +0100

    Added display of date and time

--- app/src/main/java/com/gps_usage/showCoordinates/ShowCoordinates.kt
+++ app/src/main/java/com/gps_usage/showCoordinates/ShowCoordinates.kt
@@ -11,12 +11,15 @@ import com.gps_usage.showCoordinates.data.LocationResponse
 import kotlinx.coroutines.delay
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class ShowCoordinates(getLocationSystem: () -> LocationResponse) {
     private val getLocation = getLocationSystem
@@ -26,7 +29,7 @@ class ShowCoordinates(getLocationSystem: () -> LocationResponse) {
 
         var latitude by remember { mutableDoubleStateOf(0.0) }
         var longitude by remember { mutableDoubleStateOf(0.0)}
-        var date by remember { mutableStateOf(LocalDate.now()) }
+        var date by remember { mutableStateOf(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date) }
         var time by remember { mutableStateOf(Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).time) }
 
         Box(
@@ -72,7 +75,8 @@ class ShowCoordinates(getLocationSystem: () -> LocationResponse) {
 
                 latitude = result.coordinates!!.latitude
                 longitude = result.coordinates!!.longitude
-                date =
+                date = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
+                time = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).time
 
                 delay(1000)
             }
