9d6d2950006611fa36d234cd2f18583f3d39c941
msuski <01187288@pw.edu.pl>
2025-03-06 18:52:06 +0100

    Refactor: Created LocationApp

---
+++ app/src/main/java/com/gps_usage/LocationApp.kt
@@ -0,0 +1,22 @@
+package com.gps_usage
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.gps_usage.Location.LocationService
+
+class LocationApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val channel = NotificationChannel(
+            "location",
+            "Location",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
