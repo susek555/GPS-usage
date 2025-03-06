e44643366af5e59dd81c605499e04f2ea2b6158f
msuski <01187288@pw.edu.pl>
2025-03-06 18:57:53 +0100

    Refactor: Prepared unnecessary MainActivity code to be removed

--- app/src/main/java/com/gps_usage/MainActivity.kt
+++ app/src/main/java/com/gps_usage/MainActivity.kt
@@ -27,13 +27,15 @@ import com.gps_usage.showCoordinates.data.isLocationDataSuccessful
 import com.gps_usage.ui.theme.GPSusageTheme
 
 class MainActivity : ComponentActivity() {
-    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+//    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-
+//        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        while (!checkPermissions()) {
+            requestPermissions()
+        }
         enableEdgeToEdge()
         setContent {
             GPSusageTheme {
@@ -46,57 +48,57 @@ class MainActivity : ComponentActivity() {
 
     // GPS RESPONSIBILITY
 
-    private var latitude: Double = 0.0
-    private var longitude: Double = 0.0
-    private var response: isLocationDataSuccessful = isLocationDataSuccessful.UNKNOWN_ERROR
+//    private var latitude: Double = 0.0
+//    private var longitude: Double = 0.0
+//    private var response: isLocationDataSuccessful = isLocationDataSuccessful.UNKNOWN_ERROR
+//
+//    private fun getCurrentLocation(): LocationResponse {
+//        if(checkPermissions()) {
+//            if(isLocationEnabled()) {
+//
+//                if (ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_FINE_LOCATION
+//                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION
+//                    ) != PackageManager.PERMISSION_GRANTED
+//                ) {
+//                    response = isLocationDataSuccessful.NO_PERMISSIONS
+//                }
+//                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
+//                    val location: Location?=task.result
+//                    if(location == null){
+//                        Toast.makeText(this, "Null received", Toast.LENGTH_SHORT).show()
+//                        response = isLocationDataSuccessful.UNKNOWN_ERROR
+//                    } else {
+//                        Toast.makeText(this, "Get successful", Toast.LENGTH_SHORT).show()
+//                        latitude = location.latitude
+//                        longitude = location.longitude
+//                        response = isLocationDataSuccessful.SUCCESS
+//                    }
+//
+//                }
+//            } else {
+//                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+//                val intent= Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+//                startActivity(intent)
+//                response =  isLocationDataSuccessful.LOCATION_OFF
+//            }
+//        } else {
+//            requestPermissions()
+//            response = isLocationDataSuccessful.REQUESTED_FOR_PERMISSIONS
+//        }
+//
+//        return LocationResponse(
+//            coordinates = Coordinates(latitude, longitude),
+//            response = isLocationDataSuccessful.SUCCESS
+//        )
+//    }
 
-    private fun getCurrentLocation(): LocationResponse {
-        if(checkPermissions()) {
-            if(isLocationEnabled()) {
-
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    response = isLocationDataSuccessful.NO_PERMISSIONS
-                }
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
-                    val location: Location?=task.result
-                    if(location == null){
-                        Toast.makeText(this, "Null received", Toast.LENGTH_SHORT).show()
-                        response = isLocationDataSuccessful.UNKNOWN_ERROR
-                    } else {
-                        Toast.makeText(this, "Get successful", Toast.LENGTH_SHORT).show()
-                        latitude = location.latitude
-                        longitude = location.longitude
-                        response = isLocationDataSuccessful.SUCCESS
-                    }
-
-                }
-            } else {
-                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
-                val intent= Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-                response =  isLocationDataSuccessful.LOCATION_OFF
-            }
-        } else {
-            requestPermissions()
-            response = isLocationDataSuccessful.REQUESTED_FOR_PERMISSIONS
-        }
-
-        return LocationResponse(
-            coordinates = Coordinates(latitude, longitude),
-            response = isLocationDataSuccessful.SUCCESS
-        )
-    }
-
-    companion object {
-        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
-    }
+//    companion object {
+//        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+//    }
 
     private fun checkPermissions(): Boolean {
         return (
@@ -119,37 +121,37 @@ class MainActivity : ComponentActivity() {
                 android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.ACCESS_FINE_LOCATION
             ),
-            PERMISSION_REQUEST_ACCESS_LOCATION
+            0
         )
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray,
-        deviceId: Int
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray,
+//        deviceId: Int
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+//
+//        if(requestCode == PERMISSION_REQUEST_ACCESS_LOCATION)
+//        {
+//            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
+////                getCurrentLocation()
+//            } else {
+//                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
-        if(requestCode == PERMISSION_REQUEST_ACCESS_LOCATION)
-        {
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(applicationContext, "Granted", Toast.LENGTH_SHORT).show()
-                getCurrentLocation()
-            } else {
-                Toast.makeText(applicationContext, "Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return (
-                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                        ||
-                        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-                )
-    }
+//    private fun isLocationEnabled(): Boolean {
+//        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+//        return (
+//                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+//                        ||
+//                        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+//                )
+//    }
 }
 
 
