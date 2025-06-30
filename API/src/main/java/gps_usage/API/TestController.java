59051bc99e5ce480fd20a5bfe6745963260d80b5
msuski <01187288@pw.edu.pl>
2025-06-30 19:17:12 +0200

    Configured test spring boot controller

---
+++ API/src/main/java/gps_usage/API/TestController.java
@@ -0,0 +1,12 @@
+package gps_usage.API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "This is a test message";
+    }
+}
---
+++ API/src/test/java/gps_usage/API/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gps_usage.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
