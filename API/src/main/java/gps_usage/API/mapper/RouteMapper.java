e2c26871d5aa7286292b7bc3054f49d9c23e54bd
msuski <01187288@pw.edu.pl>
2025-07-11 13:55:35 +0200

    Added necessary mapstruct processor dependency

--- API/src/main/java/gps_usage/API/mapper/RouteMapper.java
+++ API/src/main/java/gps_usage/API/mapper/RouteMapper.java
@@ -3,7 +3,6 @@ package gps_usage.API.mapper;
 import gps_usage.API.dto.RouteDTO;
 import gps_usage.API.pojo.Route;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RouteMapper {
