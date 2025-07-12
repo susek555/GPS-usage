9ad70e181436077ebd18d7633a31866b8a230167
msuski <01187288@pw.edu.pl>
2025-07-12 12:22:51 +0200

    Added config for test H2 database

---
+++ API/src/test/resources/schema.sql
@@ -0,0 +1,17 @@
+-- File: database/schema.sql
+
+CREATE TABLE routes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL DEFAULT '[ Enter name... ]',
+    numberOfPoints BIGINT NOT NULL,
+    time DATE NOT NULL
+);
+
+CREATE TABLE points (
+    id SERIAL PRIMARY KEY,
+    routeId BIGINT NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    time DATE NOT NULL,
+    FOREIGN KEY (routeId) REFERENCES routes(id) ON DELETE CASCADE
+);
