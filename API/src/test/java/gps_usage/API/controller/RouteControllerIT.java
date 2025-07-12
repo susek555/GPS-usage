9cbbf0df75123df5954eead93f35214e90f84567
msuski <01187288@pw.edu.pl>
2025-07-12 13:03:52 +0200

    Added first test

--- API/src/test/java/gps_usage/API/controller/RouteControllerIT.java
+++ API/src/test/java/gps_usage/API/controller/RouteControllerIT.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -24,7 +26,17 @@ public class RouteControllerIT {
     void testPost_AllFieldsProvided() throws Exception {
         String jsonBody = """
                 {
-                    "
+                    "name": "Test",
+                    "numberOfPoints": 10,
+                    "time": "2025-07-06"
+                }
                 """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/route/post")
+                .contentType("application/json")
+                .content(jsonBody))
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Test"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfPoints").value(10))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.time").value("2025-07-06"));
     }
 }
