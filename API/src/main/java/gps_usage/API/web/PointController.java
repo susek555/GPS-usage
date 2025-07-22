17da9c22b403c7dd571e01affd2494d1fdeb9aeb
msuski <01187288@pw.edu.pl>
2025-07-22 11:27:41 +0200

    Implemented endpoint to get paged points for routeID

--- API/src/main/java/gps_usage/API/web/PointController.java
+++ API/src/main/java/gps_usage/API/web/PointController.java
@@ -1,9 +1,11 @@
 package gps_usage.API.web;
 
 import gps_usage.API.dto.PointCreateDTO;
+import gps_usage.API.dto.PointDTO;
 import gps_usage.API.pojo.Point;
 import gps_usage.API.service.GenericService;
 import gps_usage.API.service.PointService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +21,16 @@ public class PointController extends GenericController<Point, Long>{
 
     //post no more than 200 points per single request
     @PostMapping("/post/{routeId}")
-    public ResponseEntity<Void> postPoint(@RequestBody List<PointCreateDTO> listCreateDTO, @PathVariable Long routeId) {
+    public ResponseEntity<Void> postPoints(@RequestBody List<PointCreateDTO> listCreateDTO, @PathVariable Long routeId) {
         ((PointService) service).postPoints(listCreateDTO, routeId);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{routeId}")
+    public ResponseEntity<Page<PointDTO>> getPoints(
+            @PathVariable Long routeId,
+            @RequestParam(defaultValue = "0") int pageNumber,
+            @RequestParam (defaultValue = "200") int pageSize) {
+        return new ResponseEntity<>(((PointService) service).getPointsByRouteIdPaged(routeId, pageNumber, pageSize), HttpStatus.OK);
     }
 }
