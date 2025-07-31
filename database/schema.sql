9ec9824e831e82cd870ce9189e2884ea14c827d5
Michal <01187288@pw.edu.pl>
2025-07-31 21:05:11 +0200

    Adjusted column names in database schema

--- database/schema.sql
+++ database/schema.sql
@@ -3,15 +3,15 @@
 CREATE TABLE routes (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    numberOfPoints INT NOT NULL,
+    numberofpoints INT NOT NULL,
     time DATE NOT NULL
 );
 
 CREATE TABLE points (
     id SERIAL PRIMARY KEY,
-    routeId BIGINT NOT NULL,
+    routeid BIGINT NOT NULL,
     latitude DOUBLE PRECISION NOT NULL,
     longitude DOUBLE PRECISION NOT NULL,
     time INT NOT NULL,
-    FOREIGN KEY (routeId) REFERENCES routes(id) ON DELETE CASCADE
+    FOREIGN KEY (routeid) REFERENCES routes(id) ON DELETE CASCADE
 );
