91d8bed11ae63497882a2bd0be2cb20ee9b53f4d
msuski <01187288@pw.edu.pl>
2025-10-02 23:37:24 +0200

    Added tests for get all routes paged endpoint

--- API/src/test/java/gps_usage/API/controller/RouteControllerIT.java
+++ API/src/test/java/gps_usage/API/controller/RouteControllerIT.java
@@ -12,8 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.isA;
+
 @ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
 public class RouteControllerIT {
     private final MockMvc mockMvc;
@@ -26,7 +29,6 @@ public class RouteControllerIT {
     }
 
     @Test
-    @Transactional
     void testPost_AllFieldsProvided() throws Exception {
         String jsonBody = """
                 {
@@ -45,7 +47,6 @@ public class RouteControllerIT {
     }
 
     @Test
-    @Transactional
     void testPost_OnlyNecessaryFieldsProvided() throws Exception {
 
         String jsonBody = """
@@ -84,7 +85,6 @@ public class RouteControllerIT {
     }
 
     @Test
-    @Transactional
     void testGet_RoutePresentInDatabase() throws Exception {
         String jsonBody = """
                 {
@@ -147,5 +147,81 @@ public class RouteControllerIT {
         mockMvc.perform(MockMvcRequestBuilders.delete("/route/delete/1"));
         mockMvc.perform(MockMvcRequestBuilders.delete("/route/delete/1"))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testGetAll_EmptyDatabase() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/route/get/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").isEmpty());
+    }
+
+    @Test
+    void testGetAll_SingleRoute() throws Exception {
+        String jsonBody = """
+                {
+                    "name": "Test",
+                    "numberOfPoints": 10,
+                    "time": "2025-07-06"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/route/post")
+                .contentType("application/json")
+                .content(jsonBody));
+        mockMvc.perform(MockMvcRequestBuilders.get("/route/get/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id", isA(Number.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].numberOfPoints").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].time").value("2025-07-06"));
+    }
+
+    @Test
+    void testGetAll_MultipleRoutes() throws Exception {
+        String jsonBody1 = """
+                {
+                    "name": "Test1",
+                    "numberOfPoints": 11,
+                    "time": "2025-07-07"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/route/post")
+                .contentType("application/json")
+                .content(jsonBody1));
+        String jsonBody2 = """
+                {
+                    "name": "Test2",
+                    "numberOfPoints": 12,
+                    "time": "2025-07-08"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/route/post")
+                .contentType("application/json")
+                .content(jsonBody2));
+        String jsonBody3 = """
+                {
+                    "name": "Test3",
+                    "numberOfPoints": 13,
+                    "time": "2025-07-09"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/route/post")
+                .contentType("application/json")
+                .content(jsonBody3));
+        mockMvc.perform(MockMvcRequestBuilders.get("/route/get/all"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id", isA(Number.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].name").value("Test1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].numberOfPoints").value(11))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].time").value("2025-07-07"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id", isA(Number.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].name").value("Test2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].numberOfPoints").value(12))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].time").value("2025-07-08"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id", isA(Number.class)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].name").value("Test3"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].numberOfPoints").value(13))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[2].time").value("2025-07-09"));
     }
 }
